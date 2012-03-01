978d6c28e51cebc411ce236945cb39c2c25e29a3
smit mehta <smit.meh@gmail.com>
2012-03-01 16:03:43 +0530

    final-1

--- sudoku.cpp
+++ sudoku.cpp
@@ -50,7 +50,7 @@ int fill_k(int a[][10], int o[][10][10], int u_i, int l_i, int u_j, int l_j,
 }
 
 int is_valid(int a[][10], int x, int y, int val) {
-	int i, j, sum = 0, p = (x - 1) / 3, q = (y - 1) / 3;
+	int i, j, sum = 0, p = (x - 1) / 3, q = (y - 1) / 3, b, c;
 
 	for (i = 1; i < 10; i++) {
 		if (a[x][i] == val)
@@ -72,34 +72,13 @@ int is_valid(int a[][10], int x, int y, int val) {
 
 	sum = 0;
 
-	switch (p) {
-	case 0:
-		if (q == 0)
-			return box_valid(a, 1, 4, 1, 4, val);
-		else if (q == 1)
-			return box_valid(a, 1, 4, 4, 7, val);
-		else
-			return box_valid(a, 1, 4, 7, 10, val);
-		break;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			if (i == p && j == q)
+				return box_valid(a, p * 3 + 1, p * 3 + 4, q * 3 + 1, q * 3 + 4,
+						val);
 
-	case 1:
-		if (q == 0)
-			return box_valid(a, 4, 7, 1, 4, val);
-		else if (q == 1)
-			return box_valid(a, 4, 7, 4, 7, val);
-		else
-			return box_valid(a, 4, 7, 7, 10, val);
-		break;
-
-	case 2:
-		if (q == 0)
-			return box_valid(a, 7, 10, 1, 4, val);
-		else if (q == 1)
-			return box_valid(a, 7, 10, 4, 7, val);
-		else
-			return box_valid(a, 7, 10, 7, 10, val);
-		break;
-
+		}
 	}
 
 }
@@ -144,7 +123,7 @@ void put(int a[][10], int x, int y) {
 int main() {
 
 	int a[10][10], i, h = 0, n = 0, y, j, k, x, p, q, o[10][10][10], c, d, e,
-			f = 0, g, b[10], var, K, var1, var2;
+			l, m, f = 0, g, b[10], var, K, var1, var2;
 
 	cout << "\n"
 			<< "Enter the sudoku to be solved. Enter the blank spaces as 0"
@@ -245,96 +224,21 @@ int main() {
 			p = (i - 1) / 3;
 			q = (j - 1) / 3;
 
-			switch (p) {
-			case 0:
-				if (q == 0) {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 1, 4, 1, 4, k)) {
-							fill_k(a, o, 1, 4, 1, 4, k);
-							goto start;
+			for (l = 0; l < 3; l++) {
+				for (m = 0; m < 3; m++) {
+					if (l == p && m == q) {
+						for (k = 1; k < 10; k++) {
+							if (check_box(o, p * 3 + 1, p * 3 + 4, q * 3 + 1, q
+									* 3 + 4, k)) {
+								fill_k(a, o, p * 3 + 1, p * 3 + 4, q * 3 + 1, q
+										* 3 + 4, k);
+								goto start;
+							}
 						}
 					}
 				}
-
-				else if (q == 1) {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 1, 4, 4, 7, k)) {
-							fill_k(a, o, 1, 4, 4, 7, k);
-							goto start;
-						}
-					}
-				}
-
-				else {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 1, 4, 7, 10, k)) {
-							fill_k(a, o, 1, 4, 7, 10, k);
-							goto start;
-						}
-					}
-				}
-
-				break;
-			case 1:
-				if (q == 0) {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 4, 7, 1, 4, k)) {
-							fill_k(a, o, 4, 7, 1, 4, k);
-							goto start;
-						}
-					}
-				}
-
-				else if (q == 1) {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 4, 7, 4, 7, k)) {
-							fill_k(a, o, 4, 7, 4, 7, k);
-							goto start;
-						}
-					}
-				}
-
-				else {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 4, 7, 7, 10, k)) {
-							fill_k(a, o, 4, 7, 7, 10, k);
-							goto start;
-						}
-					}
-				}
-
-				break;
-			case 2:
-				if (q == 0) {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 7, 10, 1, 4, k)) {
-							fill_k(a, o, 7, 10, 1, 4, k);
-							goto start;
-						}
-					}
-				}
-
-				else if (q == 1) {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 7, 10, 4, 7, k)) {
-							fill_k(a, o, 7, 10, 4, 7, k);
-							goto start;
-						}
-					}
-				}
-
-				else {
-					for (k = 1; k < 10; k++) {
-						if (check_box(o, 7, 10, 7, 10, k)) {
-							fill_k(a, o, 7, 10, 7, 10, k);
-							goto start;
-						}
-					}
-				}
-
-				break;
-
 			}
+
 		}
 	}
 
